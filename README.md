1b1803e03fa0bbdb1f25e92995b19f6bbc9b8d66
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-05-03 01:57:40 +0900

    tube clone

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tube
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log Out
+/users/edit -> Edit My Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> Watch Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
+/videos/comment -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.get("/", (req, res) => {
-  return res.send("I Love You");
-});
-
-app.get("/login", (req, res) => {
-  return res.send("Login here");
-});
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users/", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
