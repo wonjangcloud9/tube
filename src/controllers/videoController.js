16b5f91db32bebe02124b920207cd137c0b564b9
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-05-04 00:44:13 +0900

    make video in mongo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,7 +24,19 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
