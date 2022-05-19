742d40b4d4085c31c2ecf205540168904c31e947
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-05-19 23:54:13 +0900

    video recorder

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,2 @@
 import "../scss/styles.scss";
-console.group("main");
+import regeneratorRuntime from "regenerator-runtime";
--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,43 @@
-import regeneratorRuntime from "regenerator-runtime";
-
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const handleDowonload = () => {};
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.addEventListener("click", handleStop);
+  startBtn.removeEventListener("click", handleDowonload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    videoFile = URL.createObjectURL(e.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
