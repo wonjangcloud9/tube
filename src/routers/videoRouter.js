797d6defefc7d814a26004ef944167038b7c52cb
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-05-25 21:25:44 +0900

    upload (video & thumb) & watch (video & thumb)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -25,6 +25,12 @@ videoRouter
   .route("/upload")
   .get(getUpload)
   .all(protectorMiddleware)
-  .post(videoUpload.single("video"), postUpload);
+  .post(
+    videoUpload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumb", maxCount: 1 },
+    ]),
+    postUpload
+  );
 
 export default videoRouter;
